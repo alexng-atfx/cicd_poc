f7ffb5fc9327535be6150a2d440bf2aa5c131b47
Alex Ng <alex.ng@6317.io>
2022-12-22 09:46:28 +0800

    update build script

--- script/webhook_teams.sh
+++ script/webhook_teams.sh
@@ -6,7 +6,7 @@ if [[ "$1" == "-h" || "$1" == "--help" ]]; then
   exit 0
 fi
 
-WEBHOOK_URL=https://fxabs.webhook.office.com/webhookb2/2aa7f774-01e2-42a7-82c9-6d90520030d4@265236f7-2209-439f-9846-5423e4ae0ded/IncomingWebhook/a7b2b3ca9968420bb2e1cf631a564059/5158c2d7-f578-4d31-97e6-d46544f4ae9e
+WEBHOOK_URL=""
 TITLE="Build Success"
 MESSAGE="[📱$1] Version: $2 Release: $3<br>$4"
 COLOR="00FF00"
