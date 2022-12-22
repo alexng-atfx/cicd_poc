628ef0040ce4bbcd39a292cfc918953777cd0fcd
Alex Ng <alex.ng@6317.io>
2022-12-22 10:49:56 +0800

    update build script

--- script/webhook_teams.sh
+++ script/webhook_teams.sh
@@ -2,13 +2,14 @@
 
 # Help 
 if [[ "$1" == "-h" || "$1" == "--help" ]]; then
-  echo 'Usage: webhook_teams.sh "<Platform>" "<Version>" "<Id>" "<Download URL>"'
+  echo 'Usage: webhook_teams.sh "<Platform>" "<Version>" "<Id>" "<Download URL> "<Webhook URL>"'
   exit 0
 fi
 
+WEBHOOK_URL=$5
 TITLE="Build Success"
 MESSAGE="[📱$1] Version: $2 Release: $3<br>$4"
 COLOR="00FF00"
 JSON="{\"title\": \"${TITLE}\", \"themeColor\": \"${COLOR}\", \"text\": \"${MESSAGE}\" }"
 
-curl -H "Content-Type:application/json" -d "${JSON}" "${ENV['WEBHOOK_URL']}"
+curl -H "Content-Type:application/json" -d "${JSON}" "${WEBHOOK_URL}"
