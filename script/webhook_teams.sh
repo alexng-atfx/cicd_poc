ae74caaf4a13310e33e54bef77dae92b026f0ff1
Alex Ng <alex.ng@6317.io>
2022-12-22 15:29:58 +0800

    update build script

--- script/webhook_teams.sh
+++ script/webhook_teams.sh
@@ -6,10 +6,14 @@ if [[ "$1" == "-h" || "$1" == "--help" ]]; then
   exit 0
 fi
 
-URL=$WEBHOOK_URL
 TITLE="Build Success"
 MESSAGE="[📱$1] Version: $2 Release: $3<br>$4"
 COLOR="00FF00"
-JSON="{\"title\": \"${TITLE}\", \"themeColor\": \"${COLOR}\", \"text\": \"${MESSAGE}\" }"
 
-curl -v -H "Content-Type:application/json" -d "${JSON}" "${URL}"
+curl -f -d @- -H "Content-Type: application/json" "$WEBHOOK_URL" <<EOSLACKJSON
+{
+  "title": "$TITLE",
+  "themeColor": "$COLOR",
+  "text": "$MESSAGE"
+}
+EOSLACKJSON
